b2bba906b794c475f945f8a1a373d755ba98a862
name <flymehui@outlook.com>
2018-07-18 11:50:54 +0800

    all

--- day3.py
+++ day3.py
@@ -1,17 +1,30 @@
-# name = input('please input your name: ')
-# age = input('please input your age: ')
-#
-# print('name is %s,age is %s' % (name,age))
-#
-#
-# print("Tom's pet is a cat.The cat's name is \"Tiechui\"")
+# 1.输出名字年龄
+name = input('please input your name: ')
+age = input('please input your age: ')
 
+print('name is %s,age is %s' % (name,age))
 
-# s = "Python2best"
-# s1 = [3,5,4,2]
-# s2 = []
-# for i in s:
-#     s2.append(i)
-# print(s2)
+# 2.输出字符串
+print("Tom's pet is a cat.The cat's name is \"Tiechui\"")
 
+# 3.输出列表
+s = "Python2best"
+s1 = [3,5,4,2]
+s2 = []
+for i in s:
+    s2.append(i)
+print(s2)
 
+s2 = s2[:6]
+s2.append('3')
+print(s2)
+s2.append('!')
+print(s2)
+
+# 4.创建字典
+D = {"name":"Bob","age":"30"}
+D["score"] = "90"
+D["age"] = "25"
+age = D["age"]
+print(D)
+print("Bob's age is:",age)
--- for.py
+++ for.py
@@ -1,4 +1,29 @@
-abc = {"name":"bob"}
+# abc = {"name":"bob"}
+#
+# for names in abc:
+#     print('%s : %s' % (names,abc[names]))
+#
+# sum = 0
+# for i in range(100):
+#     sum += i
+# print(sum)
 
-for names in abc:
-    print('%s : %s' % (names,abc[names]))
+# s = int(input('please input a num: '))
+# fib = [0,1]
+# for i in range(s):
+#     fib.append(fib[-1] + fib[-2])
+# print(fib)
+
+# n = int(input('please input a number: '))
+# for i in range(1,n+1):
+#     for j in range(1,i + 1):
+#         print ('%s*%s=%s' % (j,i,i*j),end='  ')
+#     print()
+
+# print([10 + i for i in range(1,10) if i%2])
+
+f = open('/tmp/passwd')
+# data = f.read(6)
+data1 = f.readlines()
+# print(data)
+print(data1)
